374a17006868942f1dc174450f81de6c6703014a
Lucas Churchill <lucas@agtlucas.com>
2014-07-10 09:43:18 -0300

    Update app.js

--- assets/js/alpha/app.js
+++ assets/js/alpha/app.js
@@ -1,7 +1,7 @@
 var tabs = require('./who-can/tabs');
 var info = require('./who-can/info');
-var sendEmail = require('./contact/contact-form');
+var contactForm = require('./contact/contact-form');
 
 tabs;
 info;
-sendEmail;
+contactForm;
