33445a1263a641151d37b81441b9b4a468bd9cf7
Lucas Churchill <lucas@agtlucas.com>
2014-07-10 23:55:18 -0300

    Update Contact form.js

--- assets/js/alpha/contact/contact-form.js
+++ assets/js/alpha/contact/contact-form.js
@@ -19,9 +19,9 @@ var contactForm = (function (window, document, undefined) {
       message = $('.message-field').val();
 
     var info = {
-      "name": name,
-      "email": email,
-      "message": message
+      "name": "name",
+      "email": "a@a.com",
+      "message": "messages"
     };
 
     var form = $('.contact-form');
@@ -29,21 +29,22 @@ var contactForm = (function (window, document, undefined) {
 
     $(form).submit(function (event) {
 
-      $('.btn-send-message').text('Aguarde...').attr('disabled', 'disabled');
       // event.preventDefault();
       // $.ajax({
       //   type: 'POST',
       //   url: 'home/contact',
-      //   data: { "name": "Lucas", "email": 'a@a.com', "message": 'OK!!' },
+      //   data: JSON.stringify(info),
       //   contentType: 'application/json; charset=utf-8',
       //   dataType: 'json'
       // }).done(function (response) {
       //   console.log('Yeah!' + response);
-      //   console.log(info);
+      //   console.log(JSON.stringify(info));
       // }).fail(function (data) {
       //   console.log(':(' + data);
-      //   console.log(info);
+      //   console.log(JSON.stringify(info));
       // });
+
+      $('.btn-send-message').text('Aguarde...').attr('disabled', 'disabled');
     });
 
   };
