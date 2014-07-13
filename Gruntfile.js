6fc6f5802e9ce0f7c824eb460f4707281bea2e71
Lucas Churchill <lucas@agtlucas.com>
2014-07-12 21:53:55 -0300

    Update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,6 @@
 /**
  * Gruntfile
  *
- * If you created your Sails app with `sails new foo --linker`,
- * the following files will be automatically injected (in order)
- * into the EJS and HTML files in your `views` and `assets` folders.
- *
- * At the top part of this file, you'll find a few of the most commonly
- * configured options, but Sails' integration with Grunt is also fully
- * customizable.  If you'd like to work with your assets differently
- * you can change this file to do anything you like!
- *
- * More information on using Grunt to work with static assets:
- * http://gruntjs.com/configuring-tasks
  */
 
 module.exports = function (grunt) {
@@ -23,10 +12,6 @@ module.exports = function (grunt) {
    * CSS files to inject in order
    * (uses Grunt-style wildcard/glob/splat expressions)
    *
-   * By default, Sails also supports LESS in development and production.
-   * To use SASS/SCSS, Stylus, etc., edit the `sails-linker:devStyles` task
-   * below for more options.  For this to work, you may need to install new
-   * dependencies, e.g. `npm install grunt-contrib-sass`
    */
 
   var cssFilesToInject = [
@@ -38,8 +23,6 @@ module.exports = function (grunt) {
    * Javascript files to inject in order
    * (uses Grunt-style wildcard/glob/splat expressions)
    *
-   * To use client-side CoffeeScript, TypeScript, etc., edit the
-   * `sails-linker:devJs` task below for more options.
    */
 
   var jsFilesToInject = [
