d95224c12d54aac47af8b67f7bb9f7ca4f9ea052
Lucas Churchill <lucas@agtlucas.com>
2014-07-14 22:50:09 -0300

    Update flash

---
+++ api/policies/flash.js
@@ -0,0 +1,18 @@
+/**
+ * Flash Policy
+ */
+module.exports = function (req, res, next) {
+
+  res.locals.flash = {};
+
+  if (!req.session.flash) return next();
+
+  res.locals.flash = _.clone(req.session.flash);
+
+  // Clear flash
+
+  req.session.flash = {};
+
+  next();
+
+};
