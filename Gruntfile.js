4b70eb80cc12a31312d7d73abdb4ee9d0055dde7
Lucas Churchill <lucas@agtlucas.com>
2014-07-15 21:16:11 -0300

    Remove validator from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -223,7 +223,6 @@ module.exports = function (grunt) {
         files: {
           'assets/js/app.min.js': [
             'assets/bower/jquery/dist/jquery.js',
-            'assets/bower/validator-js/validator.js',
             'assets/js/alpha/**/*.js'
             ]
         }
