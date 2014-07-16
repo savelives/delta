589b85cf3713108eda602a16991369ef7f75f1f5
Lucas Churchill <lucas@agtlucas.com>
2014-07-16 02:51:23 -0300

    Update app.js

--- assets/js/alpha/app/app.js
+++ assets/js/alpha/app/app.js
@@ -2,3 +2,32 @@
  * Site stuff
  * @author Lucas <lucas@lucas.ninja>
  */
+
+var app = (function (window, document, undefined) {
+
+  'use strict';
+
+  var app = {};
+
+  app.init = function () {
+    this.scroll();
+  };
+
+  app.scroll = function () {
+    $('a[href=*=#]:not([href=#])').click(function () {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 800);
+          return false
+        }
+      }
+    });
+  };
+
+  return this.init();
+
+})(window, document);
