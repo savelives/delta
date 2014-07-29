d75fa944e5a8bdfaa0da6086cb2e1c69c7729c82
Lucas Churchill <lucas@agtlucas.com>
2014-07-29 10:16:18 -0300

    Fix iPhone 4 bug

--- assets/js/alpha/app/app.js
+++ assets/js/alpha/app/app.js
@@ -15,6 +15,7 @@ var app = (function (window, document, undefined) {
     this.polymerBigArrow();
     this.polymerMediumArrow();
     this.scrollActiveMenu();
+    this.iphoneFix();
   };
 
   app.backToTop = function () {
@@ -63,6 +64,22 @@ var app = (function (window, document, undefined) {
 
   app.scrollActiveMenu = function () {
     $.scrollIt();
+  };
+
+  app.iphoneFix = function () {
+    var iphone4 = (window.screen.height == (960 / 2));
+
+    if (iphone4) {
+      $('input, textarea').on('focus', function () {
+        $('.main-header').addClass('main-header-iphone4');
+        $('.home-part').addClass('home-part-iphone4');
+      });
+
+      $('input, textarea').on('focusout', function () {
+        $('.main-header').removeClass('main-header-iphone4');
+        $('.home-part').removeClass('home-part-iphone4');
+      });
+    };
   };
 
   return app.init();
